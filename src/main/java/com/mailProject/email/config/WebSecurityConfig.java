b6f4b73c74ea37c588703e413b059412f44c607a
ThinkBook <sukriti.sharma@tuespot.in>
2026-03-12 18:26:38 +0530

    config chnges

--- src/main/java/com/mailProject/email/config/WebSecurityConfig.java
+++ src/main/java/com/mailProject/email/config/WebSecurityConfig.java
@@ -40,7 +40,7 @@ public class WebSecurityConfig {
             "/api/schedulers/*",
             "/api/schedulers/*/toggle-status",
             "/api/schedulers/restart",
-            "/api/accounts/inbox/all"
+            "/api/accounts/inbox/all", "/api/admin/create",
     };
 
     @Bean

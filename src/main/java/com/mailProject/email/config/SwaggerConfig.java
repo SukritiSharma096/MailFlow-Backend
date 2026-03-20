f6e0e97fea2c8cde706eaa08bdf0773b892bb59d
KULDEEPSINGH-art <kuldeepsingh@tuespot.in>
2026-03-20 16:15:19 +0530

    Update SwaggerConfig.java

--- src/main/java/com/mailProject/email/config/SwaggerConfig.java
+++ src/main/java/com/mailProject/email/config/SwaggerConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.ForwardedHeaderFilter;
 import java.util.List;
 import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 
 @OpenAPIDefinition(
         servers = @Server(url = "https://jobreader.appts.in")

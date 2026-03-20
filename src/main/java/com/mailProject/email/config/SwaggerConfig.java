c980be0d6a9afdd49e8543d5adc94c5d5ea07f8e
KULDEEPSINGH-art <kuldeepsingh@tuespot.in>
2026-03-20 17:16:10 +0530

    Update SwaggerConfig.java

--- src/main/java/com/mailProject/email/config/SwaggerConfig.java
+++ src/main/java/com/mailProject/email/config/SwaggerConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.ForwardedHeaderFilter;
 import org.springframework.beans.factory.annotation.Value;
+import io.swagger.v3.oas.models.servers.Server;
 
 @Configuration
 public class SwaggerConfig {

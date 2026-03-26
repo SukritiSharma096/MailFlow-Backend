24c54b164ecf869b3257012fbab2c271b55865b9
KULDEEPSINGH-art <kuldeepsingh@tuespot.in>
2026-03-26 21:46:47 +0530

    Update SwaggerConfig.java

--- src/main/java/com/mailProject/email/config/SwaggerConfig.java
+++ src/main/java/com/mailProject/email/config/SwaggerConfig.java
@@ -35,5 +35,9 @@ public class SwaggerConfig {
                                 )
                 );
     }
+@Bean
+public ForwardedHeaderFilter forwardedHeaderFilter() {
+    return new ForwardedHeaderFilter();
+}
 }
 
